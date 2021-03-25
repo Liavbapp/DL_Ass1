ba5d2049563be8fe96ea8f97b90bee984e3112a1
Liav <liavbapp@gmail.com>
2021-03-25 17:05:08 +0200

    now forward supports Many training exampels & fixed test according

--- Tests.py
+++ Tests.py
@@ -1,9 +1,10 @@
+import random
 import unittest
 import forward
 import numpy as np
 
 
-class Tests(unittest.TestCase):
+class TestsForward(unittest.TestCase):
 
     def test_initialize_parameters_t1(self):
         dim = [3, 3, 2]
@@ -37,58 +38,77 @@ class Tests(unittest.TestCase):
         self.assertTrue(expected_dim == actual_dim)
 
     def test_linear_forward_t1(self):
-        A_prev = np.random.rand(5, )
-        W = np.random.rand(3, 5)
-        b = np.random.rand(3, )
-        expected_dim = (3,)
+        A_prev = np.random.rand(3, 7)
+        W = np.random.rand(5, 3)
+        b = np.random.rand(5, )
+        expected_dim = (5, 7)
         Z_dim = forward.linear_forward(A_prev, W, b)[0].shape
         self.assertTrue(expected_dim == Z_dim)
 
     def test_linear_forward_t2(self):
-        A_prev = np.array([0.91208099, 0.84926712, 0.34659675, 0.6648843, 0.00323112])
-        W = np.array([[0.23212763, 0.17373304, 0.97972341, 0.90126425, 0.97650933],
-                      [0.28495502, 0.61750353, 0.67666006, 0.46393482, 0.49559188],
-                      [0.56688255, 0.64774301, 0.01890737, 0.2893646, 0.74474502]])
+        A_prev = np.array([[0.91208099, 0.84926712, 0.52362],
+                           [0.33341, 0.45644, 0.11124]])  # dim: n=2 x m=3
+        W = np.array([[0.23212763, 0.17373304],
+                      [0.28495502, 0.61750353],
+                      [0.56688255, 0.64774301]])  # dim: l=3 x l-1=2
         b = np.array([4.13613966e-04, 7.75099399e-01, 6.88698967e-01])
-        expected_Z = np.array([1.30163919, 2.10401938, 1.95720217])
+        expected_Z = np.array([[0.27005715, 0.27685069, 0.14128635],
+                               [1.24088331, 1.29895564, 0.99299864],
+                               [1.42170576, 1.4657895, 1.05758494]])
         actual_Z = forward.linear_forward(A_prev, W, b)[0]
         np.testing.assert_allclose(actual_Z, expected_Z)
+        self.assertTrue(actual_Z.shape == (3, 3))
 
     def test_linear_activation_forward_t1(self):
-        A_prev = np.random.rand(5, )
-        W = np.random.rand(3, 5)
-        b = np.random.rand(3, )
+        A_prev = np.random.rand(5, 8)  # dim: n=5 x m=8
+        W = np.random.rand(7, 5)  # dim: l=7 x l-1=5
+        b = np.random.rand(7, )
         activation = "relu"
         res_cur_active, res_dict = forward.linear_activation_forward(A_prev, W, b, activation)
-        self.assertTrue(len(res_cur_active) == 3 and set(res_dict.keys()) == {'A', 'W', 'b', 'Z'})
+        self.assertTrue(res_cur_active.shape == (7, 8) and set(res_dict.keys()) == {'A', 'W', 'b', 'Z'})
 
     def test_linear_activation_forward_t2(self):
-        A_prev = np.array([0.64333428, 0.394551, 0.19479036, 0.00881926, 0.41231911])
-        W = np.array([[0.51855695, 0.30829058, 0.53081532, 0.38118337, 0.67695411],
-                      [0.00768971, 0.05420387, 0.69827948, 0.27840483, 0.50860213],
-                      [0.39872216, 0.39489627, 0.29277752, 0.10742955, 0.39920078]])
-        b = np.array([0.26883461, 0.12902709, 0.19538913])
+        A_prev = np.array([[0.25821009, 0.82192552, 0.69363923, 0.1253511, 0.79755345,
+                            0.08553971, 0.17986499, 0.96764057],
+                           [0.17007695, 0.88872916, 0.44953625, 0.02003515, 0.82340014,
+                            0.39599977, 0.49473922, 0.91576224],
+                           [0.22212011, 0.35910357, 0.23401578, 0.59454932, 0.93764331,
+                            0.59039333, 0.74805269, 0.74383216],
+                           [0.49291735, 0.00226226, 0.5712783, 0.82571755, 0.67586954,
+                            0.22033754, 0.6322879, 0.73067165],
+                           [0.63116222, 0.50653914, 0.96360902, 0.44024258, 0.26422106,
+                            0.35997056, 0.79055798, 0.60226783]])
+
+        W = np.array([[0.4780447, 0.4316493, 0.9132166, 0.95332259, 0.29922231],
+                      [0.19871669, 0.67676202, 0.03058338, 0.91666213, 0.54910392],
+                      [0.13748834, 0.74540072, 0.35554872, 0.04446443, 0.72546013],
+                      [0.32247701, 0.10526012, 0.84228229, 0.54938547, 0.85725642],
+                      [0.62944918, 0.17553011, 0.01115437, 0.58370177, 0.28079927],
+                      [0.63808557, 0.70744128, 0.51337424, 0.85654115, 0.96232575],
+                      [0.5100887, 0.10277573, 0.05485349, 0.90112344, 0.15978727]])
+
+        b = np.array([0.13685905, 0.33084046, 0.08170056, 0.76777732, 0.53512246,
+                      0.85362075, 0.46276118])
         activation = "relu"
-        expected_A = np.array([1.10995701, 0.50354015, 0.83028329])
+        expected_A = np.array([[1.19531944, 1.39505932, 1.70914452, 1.66728853, 2.45319931,
+                                1.20560484, 1.95885743, 2.55078025],
+                               [1.30245824, 1.38682789, 1.83285489, 1.38613512, 1.83987873,
+                                1.03352879, 1.73797419, 2.16611547],
+                               [0.80275176, 1.35241845, 1.31981801, 0.68135384, 1.3602298,
+                                0.86949486, 1.34281067, 1.63122846],
+                               [1.8679038, 1.86432051, 2.37579763, 2.14212544, 2.49921964,
+                                1.76396006, 2.53300818, 2.72044741],
+                               [1.19493044, 1.35604341, 1.65728741, 1.22976579, 1.66083149,
+                                0.89475159, 1.33457968, 1.90885455],
+                               [2.28231873, 2.68055115, 3.15100924, 2.38392331, 3.25957407,
+                                2.02657882, 3.00477483, 3.70619868],
+                               [1.16916614, 1.07603106, 1.54438143, 1.3757922, 1.65690379,
+                                0.83554836, 1.34247931, 1.84592371]])
         expected_dict_keys = {'A', 'W', 'b', 'Z'}
         res_A, res_dict = forward.linear_activation_forward(A_prev, W, b, activation)
-        # np.testing.assert_allclose(np.array_equal(res_A, expected_A))
         np.testing.assert_allclose(res_A, expected_A)
         self.assertTrue(set(res_dict.keys()) == expected_dict_keys)
 
-    def test_linear_activation_forward_t3(self):
-        A_prev = np.array([0.64333428, 0.394551, 0.19479036, 0.00881926, 0.41231911])
-        W = np.array([[0.51855695, 0.30829058, 0.53081532, 0.38118337, 0.67695411],
-                      [0.00768971, 0.05420387, 0.69827948, 0.27840483, 0.50860213],
-                      [0.39872216, 0.39489627, 0.29277752, 0.10742955, 0.39920078]])
-        b = np.array([0.26883461, 0.12902709, 0.19538913])
-        activation = "softmax"
-        expected_A = np.array([0.43453103, 0.23695032, 0.32851865])
-        expected_dict_keys = {'A', 'W', 'b', 'Z'}
-        res_A, res_dict = forward.linear_activation_forward(A_prev, W, b, activation)
-        # np.testing.assert_allclose(np.array_equal(res_A, expected_A))
-        np.testing.assert_allclose(res_A, expected_A)
-        self.assertTrue(set(res_dict.keys()) == expected_dict_keys)
 
     def test_apply_batchnorm_t1(self):
         A = np.array([1, 2, 3, 4, 5, 6])
@@ -104,24 +124,49 @@ class Tests(unittest.TestCase):
         np.testing.assert_allclose(expected_return, actual_return)
 
     def test_relu_t1(self):
-        Z = np.array([1, 2, 4, 5])
-        expected_res = np.array([1, 2, 4, 5])
+        Z = np.array([[1, 2, 4, 5]])
+        expected_res = np.array([[1, 2, 4, 5]])
         actual_res = forward.relu(Z)[0]
         self.assertTrue(np.array_equal(expected_res, actual_res))
 
     def test_relu_t2(self):
-        Z = np.array([1, 2, 4, -3])
-        expected_res = np.array([1, 2, 4, 0])
+        Z = np.array([[1, 2, 4, -3],
+                      [-1, 5, 8, 0]])
+        expected_res = np.array([[1, 2, 4, 0],
+                                 [0, 5, 8, 0]])
         actual_res = forward.relu(Z)[0]
         self.assertTrue(np.array_equal(expected_res, actual_res))
 
+    def test_softmax_t1(self):
+        Z = np.array([[1, 2, 4, -3],
+                      [-1, 5, 8, 0]])
+
+        actual_res = forward.softmax(Z)[0]
+        self.assertTrue(Z.shape == actual_res.shape)
+
+    def test_softmax_t2(self):
+        Z = np.array([[1, 2, 4, -3],
+                      [-1, 5, 8, 0]])
+        expected_res = np.array([[8.50660828e-04, 2.31233587e-03, 1.70859795e-02, 1.55803965e-05],
+                                 [1.15124424e-04, 4.64445075e-02, 9.32862871e-01, 3.12940630e-04]])
+        actual_res = forward.softmax(Z)[0]
+        np.testing.assert_allclose(expected_res, actual_res)
+
     def test_compute_cost(self):
         AL = np.array([[0.5, 0.3, 0.5, 0.4],
-                       [0, 0.4, 0.1, 0.1],
-                       [0.5, 0.3, 0.4, 0.4]])
-        Y = np.array([[1, 0, 0, 0],
-                      [0, 0, 0, 1],
-                      [0, 1, 1, 0]])
-        res_cost = forward.compute_cost(AL, Y)
-        predicted_cost = 0
-        self.assertTrue(res_cost == predicted_cost)
+                       [0.7, 0.4, 0.1, 0.1],
+                       [0.1, 0.3, 0.4, 0.4]])
+
+        Y = np.array([[1, 1, 0, 1],
+                      [0, 1, 0, 1],
+                      [1, 1, 1, 1]])
+        expected_cost = 2.8428564756741324
+        predicted_cost = forward.compute_cost(AL, Y)
+        np.testing.assert_allclose(expected_cost, predicted_cost)
+
+    def test_L_model_forward_t1(self):
+        X = np.random.randn(3, 6)
+        params = forward.initialize_parameters([3, 5, 7, 8, 2])
+        use_batchnorm = False
+        forward_result = forward.L_model_forward(X, params, use_batchnorm)[0]
+        self.assertTrue(forward_result.shape == (2, 6))
